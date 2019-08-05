301b1b457df8a97c3aca61c0dd59ebb922523c75
Justin Reardon <justin.reardon@gmail.com>
2019-08-05 09:35:22 -0400

    Initial production build.

---
+++ precache-manifest.512258151c69ba936f9af71f8fb2eff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3af025e17c40eb463532c0237ef24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68165acb60f42a2d4903",
+    "url": "/static/css/main.7fddbc56.chunk.css"
+  },
+  {
+    "revision": "6ecd980215bca4a16b93",
+    "url": "/static/js/2.54ae628e.chunk.js"
+  },
+  {
+    "revision": "68165acb60f42a2d4903",
+    "url": "/static/js/main.9a0ee914.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
